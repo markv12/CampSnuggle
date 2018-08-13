c89f28fdf8feea460151408f5c4d199037e3ac7b
Scott <scott_steffes@capsulatedsoftware.com>
2018-08-14 00:05:56 +0900

    Add score text

--- Assets/Scripts/EndScreenManager.cs
+++ Assets/Scripts/EndScreenManager.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 using System.Collections.Generic;
+using System;
 
 public class EndScreenManager : MonoBehaviour
 {
@@ -10,6 +11,8 @@ public class EndScreenManager : MonoBehaviour
 
     public TMP_Text scoreText;
     public TMP_Text[] highScoreTexts;
+    public TMP_Text rankText;
+    public TMP_Text nextLevelText;
 
     public Button retryButton;
     public RectTransform container;
@@ -39,6 +42,40 @@ public class EndScreenManager : MonoBehaviour
         }
 
         scoreText.text = "Score: " + score.ToString();
+        SetRankText(score);
+    }
+
+    private void SetRankText(int score)
+    {
+        string rank;
+        string next;
+        if (score < 200)
+        {
+            rank = "Bumbling Intern";
+            next = "Next Level at: 200";
+        }
+        else if (score < 500)
+        {
+            rank = "Trainee Soother";
+            next = "Next Level at: 500";
+        }
+        else if (score < 800)
+        {
+            rank = "Associate Ossan Bed Keeper";
+            next = "Next Level at: 800";
+        }
+        else if (score < 1000)
+        {
+            rank = "Professional Nuzzle Attendant";
+            next = "Next Level at: 1000";
+        }
+        else
+        {
+            rank = "God Snuggle Hustler";
+            next = "";
+        }
+        rankText.text = rank;
+        nextLevelText.text = next;
     }
 
     private void Retry()
